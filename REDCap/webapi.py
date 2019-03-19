c99bbe6a025d4c1e7ffefeeafca4372a87ae6b70
markjbaker <markdotbaker@gmail.com>
2019-03-19 14:42:27 -0400

    Continuing to outline structure

--- REDCap/webapi.py
+++ REDCap/webapi.py
@@ -1,34 +1,44 @@
 import config
+import dtypes
+import requests
 
 user_cfg = config.USER_CFG
 
-def _build_payloader(config):
-    def payloader(**kwargs):
-        payload = {
-            'token': config.token,
-            'format': config.,
-        }
-        payload.update(kwargs)
-        return payload
-    return payloader
 
-
-def send_request(**kwargs):
-    payload = _build_payload(**kwargs)
-    response = requests.post(_URL, payload)
-    if not response.ok:
-        msg = (
-            "Couldn't complete request. Code "
-            f"{response.status_code}: {response.reason}."
-        )
-        raise RedcapError(msg)
-    else:
-        return response.json()
-
-
-class RedcapRequest:
+class RedcapRequester:
     def __init__(self, cfg):
         self._config = cfg
-        self._payloader = self._build_payloader(cfg)
+        self._payloader = __class__._build_payloader(cfg)
 
-    def _build_payloader(self, cfg):
+
+    @staticmethod
+    def _build_payloader(cfg):
+        def payloader(**kwargs):
+            '''Constructs the payload for a request.'''
+            payload = {
+                'token': config['token'],
+                'url': config['url'],
+                'default_format': config['default_format'],
+            }
+            payload.update(kwargs)
+            return payload
+        return payloader
+
+
+    def send_post_request(self, **kwargs):
+        payload = self._payloader(**kwargs)
+        response = requests.post(self._config['url'], payload)
+        if not response.ok:
+            msg = (
+                "Couldn't complete request. Code "
+                f"{response.status_code}: {response.reason}."
+            )
+            raise requests.HTTPError(msg)
+        else:
+            return response.json()
+
+
+    def get_data_dictionary(self):
+        pass
+        # TODO: Implement
+        # return dtypes.DataDictionary(response_data)
